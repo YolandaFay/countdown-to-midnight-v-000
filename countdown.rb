d2dad126fc051f84c9041616fedee48b321af329
Yolanda Fay <yolandafay@gmail.com>
2018-08-07 02:50:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,9 @@ def countdown(start_num)
 end
 
 def countdown_with_sleep(start_num)
-  
-
+  while start_num != 0 
+    puts "#{start_num} SECOND(S)!" 
+    start_num -= 1 
+    sleep 1 
+  end
 end
